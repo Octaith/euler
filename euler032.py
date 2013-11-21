ff53a3a6520ab1d4d3a081865640651c84dfcaaa
Artem Kharlamov <banteeg@gmail.com>
2013-11-21 20:20:02 +0700

    problem 032 solved

---
+++ euler032.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once; for example, the 5-digit number, 15234, is 1 through 5 pandigital.
+
+The product 7254 is unusual, as the identity, 39 × 186 = 7254, containing multiplicand, multiplier, and product is 1 through 9 pandigital.
+
+Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital.
+
+HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
+'''
+import time
+
+start = time.clock()
+
+pandigital = set(str(i) for i in range(1,10))
+products = set()
+
+for a in xrange(1, 2000):
+    for b in xrange(1, 1000):
+        p = a*b
+        t = str(a)+str(b)+str(p)
+        if len(t) == 9:
+            if set(t) == pandigital:
+                products.add(p)
+
+print sum(products)
+
+print time.clock()-start
