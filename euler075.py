dc3d562084066a0fa8d1ede4d298c8486efad60c
Artem Kharlamov <banteeg@gmail.com>
2014-04-13 01:18:13 +0700

    problem 075 solved

--- euler075.py
+++ euler075.py
@@ -16,76 +16,33 @@ In contrast, some lengths of wire, like 20 cm, cannot be bent to form an integer
 Given that L is the length of the wire, for how many values of L ≤ 1,500,000 can exactly one integer sided right angle triangle be formed?
 '''
 from fractions import gcd
+from math import sqrt
 import time
-import json
+from collections import defaultdict
 start = time.clock()
 
 
-def scale(tr, m):
-    return sorted([x*m for x in tr])
-
-tops = {}
-def triangle(p):
-    if p % 2:
-        return 0
-    top = 0
-    variants = 0
-    for k in gen_asc:
-        if k > p:
-            break
-        if not p % k:
-            # m = p/k
-            # print p, k, gen[k], m, scale(gen[k], m)
-            # variants.append(gen[k])
-            tops[p] = k
-            variants += 1
-            if variants > 1:
-                return 0
-    return variants
-
-
-def triple(m, n):
-    '''http://en.wikipedia.org/wiki/Pythagorean_triple#Generating_a_triple'''
-    a = m**2 - n**2
-    b = 2*m*n
-    c = m**2 + n**2
-    p = a + b + c
-    return {p: (a, b, c)}
-
-
 def generate():
-    for m in xrange(1, limit):
+    result = 0
+    for m in xrange(1, int(sqrt(limit/2))):
         for n in xrange(1, m):
-            if (m-n) % 2 and gcd(m, n) == 1:
-                t = triple(m, n)
-                gen.update(t)
-                if t.keys()[0] > limit:
-                    print len(gen)
-                else:
-                    continue
-                break
-        else:
-            continue
-        break
+            # http://en.wikipedia.org/wiki/Pythagorean_triple#Generating_a_triple
+            if (m-n) % 2 == 1 and gcd(m, n) == 1:
+                a = m * m - n * n
+                b = 2 * m * n
+                c = m * m + n * n
+                p = a + b + c
+                while p <= limit:
+                    tris[p] += 1
+                    if tris[p] == 1:
+                        result += 1
+                    if tris[p] == 2:
+                        result -= 1
+                    p += a + b + c
+    return result
 
 limit = 1500000
-# limit = 100000
-s = 0
-gen = {}
-generate()
-gen_asc = sorted(gen.keys())
-res = 0
-itime = time.clock()
-for i in xrange(1, limit+1):
-    res += triangle(i) == 1
-    if not i % 10000:
-        print '{:10d}k{:10.3f}{:10.3f}'.format(i/1000, time.clock()-itime, time.clock()-start)
-        itime = time.clock()
+tris = defaultdict(int)
+print generate()
 
-with open('tops.json', 'w') as f:
-    stop = sorted(tops)
-    for k in stop:
-        f.write('{};{}\n'.format(k, tops[k]))
-
-print res
 print '{:1.3f}s'.format(time.clock()-start)
